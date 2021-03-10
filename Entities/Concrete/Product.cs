efc54b9915d1d01b543bd2902d9cac336fe30d01
unknown <basak>
2021-03-10 19:22:33 +0300

    first

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    // public diger katmanlar da ulaşsın diye yazılır.
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+
+    }
+}
